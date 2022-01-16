f1a71341d0d5192f32efa63ebc73a01f0817b7fd
Jeffer Marcelino <jeffermarcelinodev@gmail.com>
2022-01-16 12:56:32 +0200

    Criando swiper e fazendo alguns ajustes

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+/* Testimonials carousel slider swiper */
+const swiper = new Swiper('.swiper-container', {
+    slidesPerView: 2,
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination"
+    },
+    mousewheel: true,
+    keyboard: true,
+});
