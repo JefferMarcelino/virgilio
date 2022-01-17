ebda86380ab4576c498d99c73b43bb98f538284b
Jeffer Marcelino <jeffermarcelinodev@gmail.com>
2022-01-17 22:57:27 +0200

    Recriando o design - design não terminado

--- scripts/script.js
+++ scripts/script.js
@@ -12,10 +12,6 @@ const swiper = new Swiper('.swiper-container', {
 /* services */
 const clique = document.querySelectorAll("main section#services nav ul li")
 const ativo = document.querySelector("main section#services nav ul li.active")
-const seccaocartazes = document.querySelector("#cartazes")
-const seccaocartao = document.querySelector("#cartoes")
-const seccaocartoons = document.querySelector("#cartoons")
-const seccaologomarca = document.querySelector("#logomarca")
 
 for(const x of clique) {
     x.addEventListener("click", function () {
@@ -23,39 +19,20 @@ for(const x of clique) {
         ativo.classList.remove("active")
         x.classList.add("active")
 
-        if (clique[0].classList.contains("active")) {
+        /*if (clique[0].classList.contains("active")) {
             seccaocartazes.classList.remove("hide")
         }
 
         else {
             seccaocartazes.classList.add("hide")
         }
-
-
-        if (clique[1].classList.contains("active")) {
-            seccaocartao.classList.remove("hide")
-        }
-
-        else {
-            seccaocartao.classList.add("hide")
-        }
-
-
-        if (clique[2].classList.contains("active")) {
-            seccaocartoons.classList.remove("hide")
-        }
-
-        else {
-            seccaocartoons.classList.add("hide")
-        }
-
-
+        
         if (clique[3].classList.contains("active")) {
-            seccaologomarca.classList.remove("hide")
+            seccaologomarcas.classList.remove("hide")
         }
 
         else {
-            seccaologomarca.classList.add("hide")
-        }
+            seccaologomarcas.classList.add("hide")
+        }*/
     })
 }
