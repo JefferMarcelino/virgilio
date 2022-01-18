5c051de4a7db87a49ba5875e1049d52d3df61a39
Jeffer Marcelino <jeffermarcelinodev@gmail.com>
2022-01-18 22:26:44 +0200

    Termiando o essencial

--- scripts/script.js
+++ scripts/script.js
@@ -8,31 +8,3 @@ const swiper = new Swiper('.swiper-container', {
     mousewheel: true,
     keyboard: true,
 });
-
-/* services */
-const clique = document.querySelectorAll("main section#services nav ul li")
-const ativo = document.querySelector("main section#services nav ul li.active")
-
-for(const x of clique) {
-    x.addEventListener("click", function () {
-        const ativo = document.querySelector("main section#services nav ul li.active")
-        ativo.classList.remove("active")
-        x.classList.add("active")
-
-        /*if (clique[0].classList.contains("active")) {
-            seccaocartazes.classList.remove("hide")
-        }
-
-        else {
-            seccaocartazes.classList.add("hide")
-        }
-        
-        if (clique[3].classList.contains("active")) {
-            seccaologomarcas.classList.remove("hide")
-        }
-
-        else {
-            seccaologomarcas.classList.add("hide")
-        }*/
-    })
-}
