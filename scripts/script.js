ba29204f6e748f456705c0920a6e73b115e3aaa3
Jeffer Marcelino <jeffermarcelinodev@gmail.com>
2022-01-22 20:23:50 +0200

    Terminando a versão Desktop

--- scripts/script.js
+++ scripts/script.js
@@ -8,3 +8,34 @@ const swiper = new Swiper('.swiper-container', {
     mousewheel: true,
     keyboard: true,
 });
+
+/* Menu ativo conforme a secção visível na página */
+const sections = document.querySelectorAll("main section[id]")
+function activateMenuAtCurrentSection () {
+    const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+    for(const section of sections) {
+        const sectionTop = section.offsetTop
+        const sectionHeight = section.offsetHeight
+        const sectionId = section.getAttribute("id")
+
+        const checkpointStart = checkpoint >= sectionTop
+        const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+        if (checkpointStart && checkpointEnd) {
+            document
+                    .querySelector("header nav ul li a[href*=" + sectionId + "]")
+                    .classList.add("active")
+
+        } else {
+            document
+                .querySelector("header nav ul li a[href*=" + sectionId + "]")
+                .classList.remove("active")
+        }
+    }
+}
+
+/* When Scroll */
+window.addEventListener("scroll", function (){
+    activateMenuAtCurrentSection ()
+})
